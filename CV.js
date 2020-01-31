b992bf51a022ccb85447c0df3334e1fadaca7487
Mohammad Harba <mharba90@gmail.com>
2020-01-31 10:23:22 +0100

    alert adress and personal information

---
+++ CV.js
@@ -0,0 +1,18 @@
+const name = "Name: Mohammad Harba" ;
+const dateOfBirth = "Date of Birth: 16/6/1990" ;
+const degree = "Studies: Electrical Engineering" ;
+let experience = "Work experience: site electrical engineer" ;
+let softSkill = `Soft skills :-
+-Team Work
+-Fast learner
+-Work under pressure`
+                 
+let CV = `${name}
+${dateOfBirth}
+${degree}
+${experience}
+${softSkill}`;
+
+
+alert(CV);
+
---
+++ script.js
@@ -0,0 +1,24 @@
+//alert("Hello World");
+
+/* const firstName = "Mohammad" ;
+const lastName = "Harba" ;
+const space = " "
+let fullName = firstName + space + lastName ;
+
+const myAdress = "Via lemie, 47\n10149 Torino (TO)\n Italy" */
+
+
+let streetName = "Via leimie,";
+let civicNumber = "47" ;
+let zipCode = "10144" ;
+let cityName = "Torino" ;
+let province = "(TO)"
+let countryName = "Italy" ;
+let nl = "\n" ;
+let space = " " ;
+
+let myAdress = streetName +space + civicNumber + nl + +zipCode + space + cityName + space + province + nl + countryName ;
+
+
+
+alert(myAdress);
